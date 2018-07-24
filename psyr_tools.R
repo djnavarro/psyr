e4796b0dbe19f3a156aae3d76f089d2999d1b0be
Dani Navarro <d.navarro@unsw.edu.au>
2018-07-24 19:56:25 +1000

    commented psyr_tools

--- psyr_tools.R
+++ psyr_tools.R
@@ -1,25 +1,30 @@
 library(rmarkdown)
 library(here)
 
+# render the site in a new environment: handy to make sure 
+# that nothing in the RMD files is evaluating in .GlobalEnv
 psyr_render <- function() {
   render_site(input = here(), envir = new.env())
 }
 
+# git commit from R console
 psyr_commit <- function(message) {
   message <- paste0('"',message,'"')
   cmd <- paste(here("_shell","git_commit.sh"), message)
-  #cat(cmd)
   system(cmd)
 }
 
+# git push from R console
 psyr_push <- function() {
   system(here("_shell","git_push.sh"))
 }
 
+# git status from R console
 psyr_status <- function(){
   system(here("_shell","git_status.sh"))
 }
 
+# copy to local GAE and deploy
 psyr_deploy <- function() {
   system(here("_shell","gae_deploy.sh"))
 }
