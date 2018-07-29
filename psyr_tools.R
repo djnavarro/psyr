ebb4f3ed12a021c8feb97f00f15a441bb907dc83
Dani Navarro <d.navarro@unsw.edu.au>
2018-07-29 17:39:46 +1000

    added dates to wrangling section

--- psyr_tools.R
+++ psyr_tools.R
@@ -1,35 +1,34 @@
 library(rmarkdown)
-library(here)
 
 # render the site in a new environment: handy to make sure 
 # that nothing in the RMD files is evaluating in .GlobalEnv
 psyr_render <- function() {
-  render_site(input = here(), envir = new.env())
+  render_site(input = here::here(), envir = new.env())
 }
 
 # git commit from R console
 psyr_commit <- function(message) {
   message <- paste0('"',message,'"')
-  cmd <- paste(here("_shell","git_commit.sh"), message)
+  cmd <- paste(here::here("_shell","git_commit.sh"), message)
   system(cmd)
 }
 
 # git log from R console
 psyr_log <- function() {
-  system(here("_shell","git_log.sh"))
+  system(here::here("_shell","git_log.sh"))
 }
 
 # git push from R console
 psyr_push <- function() {
-  system(here("_shell","git_push.sh"))
+  system(here::here("_shell","git_push.sh"))
 }
 
 # git status from R console
 psyr_status <- function(){
-  system(here("_shell","git_status.sh"))
+  system(here::here("_shell","git_status.sh"))
 }
 
 # copy to local GAE and deploy
 psyr_deploy <- function() {
-  system(here("_shell","gae_deploy.sh"))
+  system(here::here("_shell","gae_deploy.sh"))
 }
